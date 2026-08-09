9c821f53863b27e548e22450c60359bd90e93d41
Fernando Trentin <73000051+fetrentin7@users.noreply.github.com>
2026-08-09 15:35:51 -0300

    perceptron test with a sigmoid function

--- perceptron.py
+++ perceptron.py
@@ -1,9 +1,10 @@
+import numpy as np
 
 x_input = [0.1, 0.5, 0.2]
 weight = [0.4, 0.3, 0.6]
 threshold = 0.5
 
-def activation(weighted_sum):
+def step(weighted_sum):
     if weighted_sum > threshold:
         return 1
     else:
@@ -16,7 +17,23 @@ def perceptron():
         weighted_sum += x_input[i] * weight[i]
         print(weighted_sum)
 
-    return activation(weighted_sum)
+    return step(weighted_sum)
+
+def sigmoid(weighted_sum):
+    output = 1 /(1+ (np.exp(-weighted_sum)))
+    return output
+
+def sigmoid_neuron():
+    weighted_sum = 0
+
+    for i in range(len(x_input)):
+        weighted_sum += x_input[i] * weight[i]
+        print(weighted_sum)
+    weighted_sum -= threshold
+    return sigmoid(weighted_sum)
 
 output = perceptron()
 print("Result: "+str(output))
+
+output = sigmoid_neuron()
+print("Result: " +str(output))
